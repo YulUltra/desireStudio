e97b7e8bc9d5efa2b51a193998afaf42a8bc5433
YulUltra <eradiateYu@gmail.com>
2023-03-03 18:49:45 +0400

    added the blog page

--- app/js/main.js
+++ app/js/main.js
@@ -19,6 +19,13 @@ $(function () {
     slidesToScroll: 8,
   });
 
+  $(".article_slider-box").slick({
+    prevArrow:
+      '<button type="button" class="article_slider_arrow article_slider_arrowleft"> <img src="images/left.svg" alt="arrow left"></button>',
+    nextArrow:
+      '<button type="button" class="article_slider_arrow article_slider_arrowright"> <img src="images/right.png" alt="arrow right"></button>',
+  });
+
   var mixer = mixitup(".gallery_inner", {
     load: {
       filter: ".category-living",
